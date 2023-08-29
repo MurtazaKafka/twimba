ae228cc4aa5fdc0e835f5d339d8ed9b930341476
Murtaza Nikzad <94598610+MurtazaKafka@users.noreply.github.com>
2023-08-29 13:40:48 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [   
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `https://unsplash.com/photos/n5aE6hOY6do`,
+        profilePic: `https://unsplash.com/photos/n5aE6hOY6do.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -13,19 +13,19 @@ export const tweetsData = [
     },    
     {
         handle: `@Elon ✅`,
-        profilePic: `https://unsplash.com/photos/rDEOVtE7vOs`,
+        profilePic: `https://unsplash.com/photos/rDEOVtE7vOs.jpg`,
         likes: 6500,
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
                   {
                 handle: `@TomCruise ✅`,
-                profilePic: `https://unsplash.com/photos/kVg2DQTAK7c`,
+                profilePic: `https://unsplash.com/photos/kVg2DQTAK7c.jpg`,
                 tweetText: `Yes! Sign me up! 😎🛩`,
             },
                   {
                 handle: `@ChuckNorris ✅`,
-                profilePic: `https://unsplash.com/photos/ILip77SbmOE`,
+                profilePic: `https://unsplash.com/photos/ILip77SbmOE.jpg`,
                 tweetText: `I went last year😴`,
             },
         ],
@@ -35,19 +35,19 @@ export const tweetsData = [
     },
         {
         handle: `@NoobCoder12`,
-        profilePic: `https://unsplash.com/photos/ILip77SbmOE`,
+        profilePic: `https://unsplash.com/photos/ILip77SbmOE.jpg`,
         likes: 10,
         retweets: 3,
         tweetText: `Are you a coder if you only know HTML?`,
         replies: [
             {
                 handle: `@StackOverflower ☣️`,
-                profilePic: `https://pin.it/1omLxkM`,
+                profilePic: `https://pin.it/1omLxkM.jpg`,
                 tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
             },
             {
                 handle: `@YummyCoder64`,
-                profilePic: `https://pin.it/1omLxkM`,
+                profilePic: `https://pin.it/1omLxkM.jpg`,
                 tweetText: `You are wonderful just as you are! ❤️`,
             },
         ],
